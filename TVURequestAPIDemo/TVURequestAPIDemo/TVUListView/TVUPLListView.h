fd451aae658232b7481f5ef8361b8afc0075ae32
sharexia <erfeixia@gmail.com>
2025-07-17 13:07:37 +0800

    add section and row

--- TVURequestAPIDemo/TVURequestAPIDemo/TVUListView/TVUPLListView.h
+++ TVURequestAPIDemo/TVURequestAPIDemo/TVUListView/TVUPLListView.h
@@ -6,42 +6,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TVUPLRowProtocol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSInteger, TVUPLListCellType) {
-    TVUPLListCellTypeDefault,
-};
-
-
-
-@class TVUPLListView, TVUPLListSection, TVUPLListItem;
-@protocol TVUPLListViewDelegate <NSObject>
-- (NSArray <NSString *>*)fetchGroupKeysWithListView:(TVUPLListView *)listView;
-- (NSDictionary *)listView:(TVUPLListView *)listView groupKey:(NSString *)key;
-
-
-@end
-
-@interface TVUPLListItem : NSObject
-
+@interface TVUPLRow : NSObject
+@property (nonatomic, assign) TVUPLRowType type;
+@property (nonatomic,   copy) NSString *key;
+@property (nonatomic, assign) UIEdgeInsets insets;
+@property (nonatomic,   copy) void (^didSelectedBlock)(TVUPLRow *row, id _Nullable value);
+@property (nonatomic,   copy) id(^fetchRowParameterBlock)(TVUPLRow *row);
+@property (nonatomic, strong, readonly) UIView <TVUPLRowProtocol> *bindView;
 @end
 
 
-@interface TVUPLListSection : UIView
+@interface TVUPLSection : NSObject
 @property (nonatomic, assign) CGFloat cornerRadius;
 @property (nonatomic, assign) UIEdgeInsets insets;
 @property (nonatomic, assign) BOOL separateLine;
 @property (nonatomic, strong) UIColor *backgroundColor;
 @property (nonatomic,   copy) NSString *key;
+@property (nonatomic, strong) UIView *bindView;
+@property (nonatomic, strong) NSMutableArray <TVUPLRow *> *rows;
 @end
 
 
 @interface TVUPLListView : UIView
-@property (nonatomic, weak) id <TVUPLListViewDelegate>delegate;
+@property (nonatomic, copy) NSArray <TVUPLSection *>*(^fetchSectionsBlock)(void);
 
 - (void)reload;
-- (void)reloadWithID:(NSString *)ID;
+- (void)reloadRowWithKey:(NSString *)key;
 @end
 
 NS_ASSUME_NONNULL_END
--- TVURequestAPIDemo/TVURequestAPIDemo/TVUListView/TVUPLListView.m
+++ TVURequestAPIDemo/TVURequestAPIDemo/TVUListView/TVUPLListView.m
@@ -6,7 +6,114 @@
 //
 
 #import "TVUPLListView.h"
+#import "NSObject+BaseDataType.h"
+#import "Masonry.h"
+
+
+NSString *const kTVUPLRowHeight = @"RowH";
+NSString *const kTVUPLRowData   = @"RowData";
+NSString *const kTVUPLRowType   = @"RowType";
+NSString *const kTVUPLRowKey    = @"RowKey";
+
+@interface TVUPLRow ()
+@property (nonatomic, strong, readwrite) UIView <TVUPLRowProtocol> *bindView;
+@end
+@implementation TVUPLRow
+@end
+
+@interface TVUPLSection ()
+
+@end
+@implementation TVUPLSection
+
+@end
+
+@interface TVUPLListView ()
+@property (nonatomic, strong) NSMutableArray <TVUPLSection *> *sections;
+@end
 
 @implementation TVUPLListView
 
+- (void)reload {
+    [self prepareFetchDatas];
+    [self layoutSubSections];
+}
+
+- (void)reloadRowWithKey:(NSString *)key {
+    
+}
+#pragma mark - Private Methods
+#pragma mark prepare data
+- (void)prepareFetchDatas {
+    if (self.fetchSectionsBlock == nil) return;
+    
+    NSArray <TVUPLSection *>*sections = self.fetchSectionsBlock();
+    if (sections.isArray == NO || sections.count == 0) {
+        return;
+    }
+    
+    for (TVUPLSection *section in sections) {
+        [self prepareForSection:section];
+    }
+    
+    self.sections = sections.mutableCopy;
+}
+
+- (void)prepareForSection:(TVUPLSection *)section {
+    if (section.rows.count == 0) return;
+    for (TVUPLRow *row in section.rows) {
+        [self prepareForRow:row];
+    }
+}
+
+- (void)prepareForRow:(TVUPLRow *)row {
+    if (row.fetchRowParameterBlock == nil) {
+        return;
+    }
+    NSDictionary *info = row.fetchRowParameterBlock(row);
+    if (info.isDictionary == NO || info.count == 0) {
+        return;
+    }
+    
+    id data = info[kTVUPLRowData];
+    UIView <TVUPLRowProtocol> *view = [self viewForRowType:row.type];
+    [view reloadWithData:data];
+    row.bindView = view;
+}
+
+- (UIView <TVUPLRowProtocol> *)viewForRowType:(TVUPLRowType)type {
+    Class cls = [self clsForRowType:type];
+    if (cls == nil) return nil;
+
+    return (UIView <TVUPLRowProtocol> *)[[cls alloc] init];
+}
+
+- (Class)clsForRowType:(TVUPLRowType)type {
+    static NSDictionary <NSNumber *, Class>*clsRowMap = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        clsRowMap = @{
+            
+        };
+    });
+    return clsRowMap[@(type)];
+}
+#pragma mark layout sections
+- (void)layoutSubSections {
+    UIView *topView = self;
+    for (TVUPLSection *section in self.sections) {
+        [self layoutForSection:section topView:topView];
+        topView = section.bindView;
+    }
+}
+
+- (void)layoutForSection:(TVUPLSection *)section topView:(UIView *)topView {
+    UIView *bindView = section.bindView;
+    [self addSubview:bindView];
+    [bindView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self).offset(section.insets.left);
+        make.top.mas_equalTo(topView).offset(section.insets.top);
+        make.right.mas_equalTo(self).offset(-section.insets.right);
+    }];
+}
 @end
